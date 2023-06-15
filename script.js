5d2e570dd849e7f8ea5a6022a9a368258df5f426
Kian <136632574+Kian-McG@users.noreply.github.com>
2023-06-15 20:56:56 +0100

    select random message function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('yay this is working')
+let messageArr = ['Cats have whiskers on the backs of their front legs, as well.',
+'Male cats are more likely to be left-pawed, while female cats are more likely to be right-pawed.',
+'A group of kittens is called a “kindle.”',
+'The first known cat video was recorded in 1894.']
+
+const ranMessage = () => {
+    let index = Math.floor(Math.random()* messageArr.length)
+    console.log(`--- your message of the day is --- \n ${messageArr[index]} `)
+}
+ranMessage()
